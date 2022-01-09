dd134ac77611f0084cb197be97ca4397e790dac3
Teamarc07 <95933650+Teamarc07@users.noreply.github.com>
2022-01-09 16:40:58 +0530

    Update logomaker.py

--- Zaid/modules/logomaker.py
+++ Zaid/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Zaid Robot](https://t.me/Zaid2_Robot)",
+            caption="Logo by [Zaid Robot](https://t.me/arc_robot)",
             force_document=False,
         )
         os.remove(flnme)
